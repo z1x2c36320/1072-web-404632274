464efd922aa9b8574012f749a5dc2a260e90f688
z1x2c36320 <z1x2c36320@gmail.com>
2019-04-17 11:50:01 +0800

    update

---
+++ hw/Assign1/js/p4.js
@@ -0,0 +1,15 @@
+function goPage(index){
+    let p = document.getElementById('section');
+    switch(index){
+        case 1:
+        p.innerHTML='<iframe width="900" height="600" src="p1/div.html" frameborder=0>';
+        berak;
+        case 2:
+        p.innerHTML='<iframe width="900" height="600" src="p2/tku60.html" frameborder=0>';
+        berak;
+        case 3:
+        p.innerHTML='<iframe width="900" height="600" src="p3/mytravel.html" frameborder=0>';
+        berak;
+        
+    }
+}
